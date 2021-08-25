e7094fc29e9d436e1f52be0cefcb477904b40735
ChaitraShetty08 <chaitrashetty948@gmail.com>
2021-08-25 16:45:30 +0530

    parameter

--- src/main/java/generic_script/BaseClass.java
+++ src/main/java/generic_script/BaseClass.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 
 import com.objectrepo.HomePage;
 import com.objectrepo.Loginpage;
@@ -22,7 +21,7 @@ public class BaseClass
 	public Loginpage lp=new Loginpage(d);
 	public JavaUtilities r=new JavaUtilities();
 
-	@Parameters("BROWSER")
+	//@Parameters("BROWSER")
 	
 	@BeforeClass(groups={"Regressiontest"})
 	public void launchBrowser() throws IOException
