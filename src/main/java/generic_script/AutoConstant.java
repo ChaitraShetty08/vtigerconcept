c63e0d67e61883e7fbebcb6d81710ddc6e08c2a4
ChaitraShetty08 <chaitrashetty948@gmail.com>
2021-08-25 16:12:55 +0530

    updated one

--- src/main/java/generic_script/AutoConstant.java
+++ src/main/java/generic_script/AutoConstant.java
@@ -5,7 +5,7 @@ public interface AutoConstant
 	public static String DirPath=System.getProperty("user.dir");
 	
 	
-	String propfile=DirPath+"/vtigerconcept/CommonData.propeties";
+	String p=DirPath+"/vtigerconcept/CommonData.propeties";
 	String JSONfilepath="";
 	String XMLfilepath="";
 	String Excelpath="";
