e4bf6525e1ad97bd4d096cfd9784eaa5e3e142ff
ChaitraShetty08 <chaitrashetty948@gmail.com>
2021-08-26 11:38:19 +0530

    groupssmoke

--- src/test/java/vtigerTestCases/TC10.java
+++ src/test/java/vtigerTestCases/TC10.java
@@ -63,5 +63,57 @@ public class TC10 extends BaseClass
 
 
 	}	
+	
+	@Test(groups="SmokeTest")
+	public void testCase11() throws IOException, InterruptedException
+	{
+		
+		HomePage hp=new HomePage(d);
+		hp.moreLinkInHomePage(d);
+
+		CreateSalesOrderPage sales=new CreateSalesOrderPage(d);
+		sales.getCreatesalesbtn().click();
+		sales.getSubjecttxtfld().sendKeys("Samsungj7"); 	
+		sales.getOrgimgbtn().click();
+		webu.switchwindow(d, "Accounts&action");
+		sales.getSearchordnametxtfld().sendKeys("Samsung");
+		sales.getSearchnowbtn().click();
+		sales.getOrgname().click();
+		webu.alterPopup(d);
+		Thread.sleep(3000);
+		webu.switchwindow(d, "SalesOrder&action");
+		Thread.sleep(3000);
+
+		webu.scrollTo(d, sales.getBillingaddtxtareafld());
+		Thread.sleep(3000);
+
+		sales.getBillingaddtxtareafld().sendKeys(p.readData("billingadd"));
+		sales.getShippingaddtxtareafld().sendKeys(p.readData("shippingadd"));
+
+		webu.scrollTo(d, sales.getStatusdropdown());
+
+		webu.select_DD(sales.getStatusdropdown(), 0);
+		sales.getAssignedtouserradiobtn().click();
+		webu.select_DD(sales.getAssignedtouserdropdown(), 0);
+		Thread.sleep(3000);
+
+		webu.scrollTo(d, sales.getInvoicestatusdropdown());
+		webu.select_DD(sales.getInvoicestatusdropdown(), 5);
+
+		webu.scrollTo(d, sales.getItemnamelookupimg());
+		sales.getItemnamelookupimg().click();
+		Thread.sleep(3000);
+
+		webu.switchwindow(d, "Products&action");
+		sales.getProductnamelink().click();
+		Thread.sleep(3000);
+		webu.switchwindow(d, "Sales");
+		Thread.sleep(3000);
+		sales.getQtytxtfld().sendKeys("40");
+		Thread.sleep(3000);
+		//sales.getSavebtn().click();
+
+		
+	}
 
 }
